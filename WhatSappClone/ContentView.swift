852d2d6c3de623317f7d598c3cdb1aea843a7268
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2024-10-19 13:44:39 +0700

    Initial Commit

---
+++ WhatSappClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 19/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatSappClone/WhatSappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatSappCloneApp.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatSappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

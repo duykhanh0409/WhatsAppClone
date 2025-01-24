880e38e6d582624854c2c404cbd56a31b3449774
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-01-24 12:10:04 +0700

    feat: create Communities tab screen

---
+++ WhatSappClone/Screens/Communitity/Views/CommunityTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityTabScreen.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 24/1/25.
+//
+
+import SwiftUI
+
+struct CommunityTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityTabScreen()
+}

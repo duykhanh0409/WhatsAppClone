db0f4525517541430a3d6b086b21df8e6fa7ffa5
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-01-25 18:39:06 +0700

    chore: update login and signup screen

--- WhatSappClone/Screens/Auth/ViewModels/AuthScreenModel.swift
+++ WhatSappClone/Screens/Auth/ViewModels/AuthScreenModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+
+final class AuthScreenModel: ObservableObject {
+    
+    // MARK: Published Properties
+
+    @Published var isLoading = false
+    @Published var email = ""
+    @Published var password = ""
+    @Published var username = ""
+    
+    // MARK: Computed Properties
+    var disableLoginButton: Bool {
+        return email.isEmpty || password.isEmpty || isLoading
+    }
+    
+    var disableSignUpButton: Bool {
+        return email.isEmpty || password.isEmpty || username.isEmpty || isLoading
+    }
+}
+
--- WhatSappClone/Screens/Auth/Views/LoginScreen.swift
+++ WhatSappClone/Screens/Auth/Views/LoginScreen.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    @StateObject private var authScreenModel = AuthScreenModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                AuthHeaderView()
+                
+                AuthTextField(type: .email, text: $authScreenModel.email)
+                AuthTextField(type: .password, text: $authScreenModel.password)
+                
+                forgotPasswordButton()
+                
+                AuthButton(title: "Log in now") {
+                    //
+                }
+                .disabled(authScreenModel.disableLoginButton)
+                
+                Spacer()
+                
+                signUpButton()
+                    .padding(.bottom, 30)
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.teal.gradient)
+            .ignoresSafeArea()
+        }
+    }
+    
+    private func forgotPasswordButton() -> some View {
+        Button {
+            
+        } label: {
+            Text("Forgot Password ?")
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing, 32)
+                .bold()
+                .padding(.vertical)
+            
+        }
+    }
+    
+    private func signUpButton() -> some View {
+        NavigationLink {
+            SignUpScreen(authScreenModel: authScreenModel)
+            
+        } label: {
+            HStack {
+                Image(systemName: "sparkles")
+                
+                (
+                
+                    Text("Don't have an account ? ")
+                    +
+                    Text("Create one").bold()
+                
+                )
+                Image(systemName: "sparkles")
+            }
+            .foregroundStyle(.white)
+        }
     }
 }
 

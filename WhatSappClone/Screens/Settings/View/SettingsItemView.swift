0775c746e58f9788762926e1f842f72145696786
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-01-25 13:57:24 +0700

    feat: build setting tab page

---
+++ WhatSappClone/Screens/Settings/View/SettingsItemView.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsItemView.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 25/1/25.
+//
+
+import SwiftUI
+
+struct SettingsItemView: View {
+    let item: SettingsItem
+    var body: some View {
+        HStack{
+            
+            iconImageView()
+                .frame(width: 30, height: 30)
+                .foregroundStyle(.white)
+                .background(item.backgroundColor)
+                .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
+            
+            Text(item.title)
+                .font(.system(size: 18))
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private func iconImageView() -> some View {
+        switch item.imageType {
+        case .systemImage:
+            Image(systemName: item.imageName)
+                .bold()
+                .font(.callout)
+            
+        case .assetImage:
+            Image(item.imageName)
+                .renderingMode(.template)
+                .padding(3)
+        }
+    }
+}
+
+#Preview {
+    SettingsItemView(item: .avatar)
+}

48b8a032a553511e48cb181aff0f457493f0e3af
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-03-18 21:40:27 +0700

    feat: build CircularProfileImageView component

---
+++ WhatSappClone/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularImageView.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 18/3/25.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularImageView()
+}

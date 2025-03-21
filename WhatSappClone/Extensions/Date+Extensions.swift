7bf082221c811afd6cce279645ac22cc36350e39
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-03-21 07:27:47 +0700

    chore: create extensions date

---
+++ WhatSappClone/Extensions/Date+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Date+Extensions.swift
+//  WhatSappClone
+//
+//  Created by Khanh Nguyen on 18/3/25.
+//
+
+import Foundation
+
+extension Date {
+    var dayOrTimeRepresentation: String {
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        
+        if calendar.isDateInToday(self){
+            dateFormatter.dateFormat = "h:mm a"
+            let formattedDate = dateFormatter.string(from: self)
+            return formattedDate
+        } else if calendar.isDateInToday(self){
+            return "Yesterday"
+        } else {
+            dateFormatter.dateFormat = "MM/dd/yy"
+            return dateFormatter.string(from: self)
+        }
+        
+    }
+}
